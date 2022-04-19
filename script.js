c355a7a56f9f3b872114a48e3363d59f43e9af0d
PraveenKannan14 <82314382+PraveenKannan14@users.noreply.github.com>
2022-04-19 06:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function page() {
-  location.href('https://www.youtube.com/');
-};
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
